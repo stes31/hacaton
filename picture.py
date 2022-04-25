4da65d8cc8d3b69844415c8bbf6c1c368184d775
bulat <bulatnikov.i@mail.ru>
2022-04-26 00:13:40 +0300

    start neiron

---
+++ picture.py
@@ -0,0 +1,23 @@
+import bitmap as b
+
+
+class FirstObr:
+
+    def __init__(self):
+        pass
+
+    def open_from_file(self, filename):
+        pass
+
+    def convert_to_coo(self):
+        pass
+        # вместо return
+        self.coords = (x, y)
+        self.color = (r, g, b)
+
+
+
+
+
+
+
