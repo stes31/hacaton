55233ddb5bd5584a9eee059e275218bc738348d8
bulat <bulatnikov.i@mail.ru>
2022-04-25 21:14:21 +0300

    ky

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-if __name__ == "main.py":
-    pass
+import bitmap as b
+
+
+class FirstObr:
+
+    def __init__ (self):
+        pass
+
+    def open_from_file(self, filename):
+        pass
+
+    def convert_to_coo(self, ):
 
