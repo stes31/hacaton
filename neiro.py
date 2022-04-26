207e3c880c08bd1c2ebcd6785e8fdaf0b838020a
bulat <bulatnikov.i@mail.ru>
2022-04-26 03:07:23 +0300

    1

---
+++ neiro.py
@@ -0,0 +1,42 @@
+from picture import *
+import numpy as np
+from random import random as r
+
+class Neirons:
+
+    def __init__(self):
+        self.inputs = 3
+        self.hides_levels = 2
+        self.hiden = 6
+        self.outputs = 3
+        self.weights = [
+
+            # input-hide
+            np.array(
+                [r(),r(),r()],
+                [r(),r(),r()],
+                [r(),r(),r()],
+                [r(),r(),r()],
+                [r(),r(),r()],
+                [r(),r(),r()]),
+
+            # hide-hide
+            np.array(
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()]),
+
+            # hide-output
+            np.array(
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()],
+                [r(),r(),r(),r(),r(),r()])]
+
+    def neiron(self, x):
+        for layer in range(self.hides_levels + 1):
+            x = np.matmul(self.weights(layer), x)
+        pass
+
