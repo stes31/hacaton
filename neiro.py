2d685fcab7d9d68d6bc0bf1d5413087c8d018bf2
bulat <bulatnikov.i@mail.ru>
2022-04-26 19:29:56 +0300

    2

--- neiro.py
+++ neiro.py
@@ -2,9 +2,11 @@ from picture import *
 import numpy as np
 from random import random as r
 
+
 class Neirons:
 
     def __init__(self):
+        self.neiro = None
         self.inputs = 3
         self.hides_levels = 2
         self.hiden = 6
@@ -13,30 +15,34 @@ class Neirons:
 
             # input-hide
             np.array(
-                [r(),r(),r()],
-                [r(),r(),r()],
-                [r(),r(),r()],
-                [r(),r(),r()],
-                [r(),r(),r()],
-                [r(),r(),r()]),
+                [r(), r(), r()],
+                [r(), r(), r()],
+                [r(), r(), r()],
+                [r(), r(), r()],
+                [r(), r(), r()],
+                [r(), r(), r()]),
 
             # hide-hide
             np.array(
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()]),
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()]),
 
             # hide-output
             np.array(
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()],
-                [r(),r(),r(),r(),r(),r()])]
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()],
+                [r(), r(), r(), r(), r(), r()])]
 
     def neiron(self, x):
         for layer in range(self.hides_levels + 1):
-            x = np.matmul(self.weights(layer), x)
+            x = np.matmul(self.weights[layer], x)
+        self.neiro = x
+
+    def learn(self):
         pass
+    # обучение
 
